1062923c6298cc0ff65960479b2b220e5cd49c4f
Hugh Naaek <94478009+hughnaaek@users.noreply.github.com>
2022-04-17 10:19:22 +0700

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -5,8 +5,14 @@ import streamlit as st
 
 def simple_kmeans(training_df):
     
-    kmeans = KMeans(n_clusters=8)
-    identified_clusters = kmeans.fit_predict(training_df)
+    std = StandardScaler()
+    pca = PCA(n_components=20)
+    kmeans = KMeans(n_clusters=4, init='k-means++', n_init=10, max_iter=100, random_state=42)
+
+    
+    df_new = std.fit_transform(training_df)
+    df_new = pca.fit_transform(df_new)
+    identified_clusters = kmeans.fit_predict(df_new)
     # identified_clusters.rename(columns={"0": "group"})
 
     return identified_clusters
