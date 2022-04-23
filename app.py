fd18e03592c5165378a1495bc649e6ea43e0a961
Hugh Naaek <94478009+hughnaaek@users.noreply.github.com>
2022-04-23 18:22:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app2 = MultiPage2()
 
 
 display1 = Image.open('Logo1.png')
-st.image(display1, width = 650)
+st.image(display1, width = 350)
 display2 = Image.open('Logo2.png')
-st.image(display2, width = 650)
+st.image(display2, width = 500)
 
 st.title('Welcome to AnnaMatching')
 st.header("The Best platform to find your best partner")
